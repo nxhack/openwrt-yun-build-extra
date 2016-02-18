e806543158e72f187fa0093afb1c19f7621cbc43
Hirokazu MORIKAWA <register@nxhack.com>
2016-02-18 20:24:48 +0900

    initial commit

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -ex
+
+#FEEDS
+./scripts/feeds uninstall -a
+rm -rf feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+#UNINSTALL BROKEN PACKAGES
+#./scripts/feeds uninstall aria2 freecwmp libfreecwmp libmicroxml
+
+#DELETE PACKAGES
+rm -rf ./package/feeds/packages/node-serialport
+rm -rf ./package/feeds/packages/node-arduino-firmata
+
+#LINK CUSTOM PACKAGES
+ln -s ../../../feeds/arduino/node-serialport ./package/feeds/packages/
+ln -s ../../../feeds/arduino/node-arduino-firmata ./package/feeds/packages/
+
+# PATCH PACKAGES
+sed -i -e s/^START=98/START=48/ ./feeds/packages/utils/rng-tools/files/rngd.init
+
+#COPY CONFIG FILE
+mv .config .config.$$
+cp openwrt-yun-minimum.config .config
+
+make oldconfig
+make
