96cebd47f8acdf4f59682a0305221f26942db5be
Hirokazu MORIKAWA <register@nxhack.com>
2016-02-26 12:59:03 +0900

    backup local build key

--- build.sh
+++ build.sh
@@ -1,5 +1,33 @@
 #!/bin/bash -ex
 
+BUILD_DATE=`date +%Y%m%d-%H%M%S`
+
+#CREATE BACKUP DIRECTORY
+if [ ! -e 'backups' ]; then
+    mkdir backups
+fi
+
+#BACKUP AND RESTORE LOCAL BUILD KEY
+if [ -e 'key-build' ]; then
+    cp -n key-build ./backups/
+    cp key-build ./backups/key-build.${BUILD_DATE}-$$
+else
+    if [ -e 'backups/key-build' ]; then
+        cp ./backups/key-build .
+    fi
+fi
+if [ -e 'key-build.pub' ]; then
+    cp -n key-build.pub ./backups/
+    cp key-build.pub ./backups/key-build.pub.${BUILD_DATE}-$$
+else
+    if [ -e 'backups/key-build.pub' ]; then
+        cp ./backups/key-build.pub .
+    fi
+fi
+
+#CLEAN
+make clean
+
 #FEEDS
 ./scripts/feeds uninstall -a
 rm -rf feeds
@@ -19,12 +47,12 @@ ln -s ../../../feeds/arduino/node-arduino-firmata ./package/feeds/arduino/
 
 # PATCH PACKAGES
 sed -i -e s/^START=98/START=48/ ./feeds/packages/utils/rng-tools/files/rngd.init
-if [ -z "`git status|fgrep mach-arduino-yun.c`" ]; then
+if [ -z '`git status|fgrep mach-arduino-yun.c`' ]; then
     patch -p1 < ./patches/00-arduino-yun-gpio_keys_polled.patch
 fi
 
 #COPY CONFIG FILE
-mv .config .config.$$
+mv .config ./backups/config.${BUILD_DATE}-$$
 cp openwrt-yun-minimum.config .config
 
 make oldconfig
