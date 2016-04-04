9350638da1fe6662f2db49f331d88d5f1dd2a4bc
Hirokazu MORIKAWA <register@nxhack.com>
2016-04-04 23:28:40 +0900

    remove node installer

--- FIRST_SETUP_ubuntu_Trusty_Tahr.sh
+++ FIRST_SETUP_ubuntu_Trusty_Tahr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright (c) 2014 Arduino LLC. All right reserved.
+# Copyright (c) 2014-2016 Arduino LLC. All right reserved.
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -15,10 +15,5 @@ sudo apt-get install -y git subversion build-essential asciidoc \
 	bison libboost-dev libxml-parser-perl libusb-dev bin86 \
 	bcc sharutils openjdk-7-jdk mercurial cvs bzr \
 	nodejs-legacy curl g++-multilib squashfs-tools
-
-#### node-* package do node/host install, so not required nodejs install process.
-# Install nodejs : https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
-#curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-#sudo apt-get install -y nodejs
 
 echo "ALL DONE! YEAH!!"
