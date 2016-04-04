0d0fe9fb3355b52fa59823e3eb12f16dfdac563e
Hirokazu MORIKAWA <register@nxhack.com>
2016-04-04 17:18:38 +0900

    host setup: remove nodejs installer

--- FIRST_SETUP_ubuntu_Trusty_Tahr.sh
+++ FIRST_SETUP_ubuntu_Trusty_Tahr.sh
@@ -16,7 +16,7 @@ sudo apt-get install -y git subversion build-essential asciidoc \
 	bcc sharutils openjdk-7-jdk mercurial cvs bzr \
 	nodejs-legacy curl g++-multilib squashfs-tools
 
-#### node-* package do host install, so not required nodejs install process.
+#### node-* package do node/host install, so not required nodejs install process.
 # Install nodejs : https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
 #curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 #sudo apt-get install -y nodejs
