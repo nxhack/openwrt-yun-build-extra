61d7e544762d293d677126006a8ecb50e1f56455
Hirokazu MORIKAWA <register@nxhack.com>
2018-08-15 08:49:44 +0900

    rebuild.sh: modify

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,23 @@
 
 git pull
 ./scripts/feeds update -a
-#./scripts/feeds install -a
-make oldconfig
+./scripts/feeds install -a
+
+#DELETE OPENWRT NODE PACKAGES
+rm  ./package/feeds/packages/node
+rm  ./package/feeds/packages/node-*
+
+#INSTALL CUSTOM NODE PACKAGES
+./scripts/feeds install -a -p node
+
+#DELETE OPENWRT MRAA PACKAGES
+rm ./package/feeds/packages/libmraa
+rm ./package/feeds/packages/libupm
+
+#INSTALL CUSTOM MRAA PACKAGES
+./scripts/feeds install -a -p inteliot
+
+cp openwrt-yun-lininoos.diffconfig .config
+
+make defconfig
 make
