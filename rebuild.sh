c3888f41479b92784662d554b13d2152294a1655
Hirokazu MORIKAWA <register@nxhack.com>
2018-08-15 08:53:25 +0900

    rebuild.sh: modify

--- rebuild.sh
+++ rebuild.sh
@@ -5,15 +5,15 @@ git pull
 ./scripts/feeds install -a
 
 #DELETE OPENWRT NODE PACKAGES
-rm  ./package/feeds/packages/node
-rm  ./package/feeds/packages/node-*
+rm  -f ./package/feeds/packages/node
+rm  -f ./package/feeds/packages/node-*
 
 #INSTALL CUSTOM NODE PACKAGES
 ./scripts/feeds install -a -p node
 
 #DELETE OPENWRT MRAA PACKAGES
-rm ./package/feeds/packages/libmraa
-rm ./package/feeds/packages/libupm
+rm -f ./package/feeds/packages/libmraa
+rm -f ./package/feeds/packages/libupm
 
 #INSTALL CUSTOM MRAA PACKAGES
 ./scripts/feeds install -a -p inteliot
