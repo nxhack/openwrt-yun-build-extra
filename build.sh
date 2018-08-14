501bd989646f7bb9e9ccfb235cc658b8ea8980e2
Hirokazu MORIKAWA <register@nxhack.com>
2018-08-15 08:59:26 +0900

    modify

--- build.sh
+++ build.sh
@@ -53,16 +53,16 @@ rm -rf feeds
 ./scripts/feeds install -a
 
 #DELETE OPENWRT NODE PACKAGES
-rm  ./package/feeds/packages/node
-rm  ./package/feeds/packages/node-*
+rm -f ./package/feeds/packages/node
+rm -f ./package/feeds/packages/node-*
 
 #INSTALL CUSTOM NODE PACKAGES
 ./scripts/feeds install -a -p node
 
 #DELETE OPENWRT MRAA PACKAGES
-rm ./package/feeds/packages/swig
-rm ./package/feeds/packages/libmraa
-rm ./package/feeds/packages/libupm
+rm -f ./package/feeds/packages/swig
+rm -f ./package/feeds/packages/libmraa
+rm -f ./package/feeds/packages/libupm
 
 #INSTALL CUSTOM MRAA PACKAGES
 ./scripts/feeds install -a -p inteliot
--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,24 @@
 
 git pull
 ./scripts/feeds update -a
-#./scripts/feeds install -a
-make oldconfig
+./scripts/feeds install -a
+
+#DELETE OPENWRT NODE PACKAGES
+rm  -f ./package/feeds/packages/node
+rm  -f ./package/feeds/packages/node-*
+
+#INSTALL CUSTOM NODE PACKAGES
+./scripts/feeds install -a -p node
+
+#DELETE OPENWRT MRAA PACKAGES
+rm -f ./package/feeds/packages/swig
+rm -f ./package/feeds/packages/libmraa
+rm -f ./package/feeds/packages/libupm
+
+#INSTALL CUSTOM MRAA PACKAGES
+./scripts/feeds install -a -p inteliot
+
+cp openwrt-yun-lininoos.diffconfig .config
+
+make defconfig
 make
